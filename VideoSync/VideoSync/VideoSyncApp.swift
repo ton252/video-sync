75ada795b45b45c6ca81ebb26eca622b1b1033ee
Anton Poliakov <tonwork252@gmail.com>
2024-04-09 21:55:05 +0300

    Add project

---
+++ VideoSync/VideoSync/VideoSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoSyncApp.swift
+//  VideoSync
+//
+//  Created by anton.poliakov on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
